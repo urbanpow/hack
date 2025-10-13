b6f8efc8a4a2326631f3dc22e00183448ae7146a
Satvik Bhatnagar <bhatnagarsatvik27@gmail.com>
2025-10-14 00:43:36 +0530

    C++ Sudoku Solver using Backtracking

---
+++ SudokuSolver.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 9;
+
+// Function to print the board
+void printBoard(int board[SIZE][SIZE]) {
+    for (int row = 0; row < SIZE; ++row) {
+        for (int col = 0; col < SIZE; ++col)
+            cout << board[row][col] << " ";
+        cout << endl;
+    }
+}
+
+// Check if placing num at board[row][col] is valid
+bool isValid(int board[SIZE][SIZE], int row, int col, int num) {
+    for (int x = 0; x < SIZE; ++x) {
+        if (board[row][x] == num || board[x][col] == num)
+            return false;
+    }
+
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            if (board[startRow + i][startCol + j] == num)
+                return false;
+
+    return true;
+}
+
+// Recursive backtracking solver
+bool solveSudoku(int board[SIZE][SIZE]) {
+    for (int row = 0; row < SIZE; ++row) {
+        for (int col = 0; col < SIZE; ++col) {
+            if (board[row][col] == 0) {
+                for (int num = 1; num <= 9; ++num) {
+                    if (isValid(board, row, col, num)) {
+                        board[row][col] = num;
+                        if (solveSudoku(board))
+                            return true;
+                        board[row][col] = 0; // backtrack
+                    }
+                }
+                return false; // no valid number found
+            }
+        }
+    }
+    return true; // board is complete
+}
+
+int main() {
+    int board[SIZE][SIZE] = {
+        {5,3,0,0,7,0,0,0,0},
+        {6,0,0,1,9,5,0,0,0},
+        {0,9,8,0,0,0,0,6,0},
+        {8,0,0,0,6,0,0,0,3},
+        {4,0,0,8,0,3,0,0,1},
+        {7,0,0,0,2,0,0,0,6},
+        {0,6,0,0,0,0,2,8,0},
+        {0,0,0,4,1,9,0,0,5},
+        {0,0,0,0,8,0,0,7,9}
+    };
+
+    if (solveSudoku(board)) {
+        cout << "Solved Sudoku:\n";
+        printBoard(board);
+    } else {
+        cout << "No solution exists.\n";
+    }
+
+    return 0;
+}
